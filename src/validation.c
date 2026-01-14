c633e3efdebfed0e77adcf694c58cd6958825fc9
Jakub Cirip <101516870+jakubcirip@users.noreply.github.com>
2026-01-14 21:46:29 +0100

    Add functions validate_row() and validate_col()

--- src/validation.c
+++ src/validation.c
@@ -3,19 +3,35 @@
 bool validate_row(const struct sudoku *s, int row){
     int seen[10] = {0};
     for(int i=0; i<SIZE; i++){
-        if(board_get(s, row, i) == ' '){
+        char c = board_get(s, row, i);
+        if(c == ' '){
             continue;
         }
-        else {
-            int num = board_get(s, row, i) - '0';
-            seen[num]++;
-            
+        int num = c - '0';
+        seen[num]++;
+        if(seen[num]>1){
+            return false;
         }
     }
+    return true;
 }
 
 // Column validation
-bool validate_col(const struct sudoku *s, int col);
+bool validate_col(const struct sudoku *s, int col){
+    int seen[10] = {0};
+    for(int i=0; i<SIZE; i++){
+        char c = board_get(s, i, col);
+        if(c == ' '){
+            continue;
+        }
+        int num = c - '0';
+        seen[num]++;
+        if(seen[num]>1){
+            return false;
+        }
+    }
+    return true;
+}
 
 // 3x3 blocks validation
 bool validate_block(const struct sudoku *s, int block_row, int block_col);
