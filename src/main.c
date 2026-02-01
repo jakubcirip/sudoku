aaf31758f7a3b0235b9a6d103387cfd65cab37c8
Jakub Cirip <jakub@jakub.mooz7>
2026-02-01 19:15:56 +0100

    main.c update

--- src/main.c
+++ src/main.c
@@ -3,12 +3,6 @@
 #include "board.h"
 #include "render.h"
 
-/*typedef struct {
-    char board[9][9];
-    bool initial[9][9];
-} Sudoku;
-*/
-
 int main(){
     Sudoku *ptr = malloc(sizeof(Sudoku));
     if(ptr == NULL){
