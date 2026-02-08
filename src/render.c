9e6a0a0b2d4dcaf56bd6ba0ddc92818d8f5bfedc
Jakub Cirip <jakub@jakub.mooz7>
2026-02-08 13:29:02 +0100

    Add colors to the game render

--- src/render.c
+++ src/render.c
@@ -12,9 +12,12 @@ void render_board(const Sudoku *s){
             if(col == 0 || col == 3 || col == 6)
                 printf("| ");
             char c = board_get(s, row, col);
-	    if(c == '0' || c == 0)
-		c = '.';
-            printf("%c ", c);
+	    if(c == '.' || c == 0)
+            	printf("\033[90m.\033[0m ");
+	    else if (s->initial[row][col])
+		printf("\033[1;33m%c\033[0m ", c);
+	    else
+		printf("\033[37m%c\033[0m ", c);
         }
         printf("| %d\n", row+1);
     }
